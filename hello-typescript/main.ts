633d8a8117d2ca5a65a7e12f292cd675a0925931
Rafael Tavares <rafael@serveloja.com.br>
2017-02-15 10:57:57 -0300

    estudando typescript

---
+++ hello-typescript/main.ts
@@ -0,0 +1,19 @@
+var minhaVar = 'minha variavel';
+
+//ES 6 ES 2015
+let num = 2;
+const PI = 3.14;
+
+//JS
+var numeros = [1,2,3];
+numeros.map(function(valor ){ return valor* 2;})
+
+//Arrow functions
+var numeros = [1,2,3];
+numeros.map(valor =>  valor * 2);
+
+class Matematica { 
+    soma(x,y){
+        return x + y;
+    }
+}
